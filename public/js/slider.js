2930e94aee9572a3253cfbf52080ff2d92f8bd3b
Ildar Mullakhmetov <ildarum.work@yandex.ru>
2020-07-05 22:56:35 +0400

    123

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-  $(".footerSlider").slick({
-    infinite:false,
-    initialSlide:1,
-    centerMode:true,
-    dots:true,
-    slidesToShow: 1,
-  });
+$(document).ready(function() {
+    $(".footerSlider").slick({
+        infinite: false,
+        initialSlide: 1,
+        centerMode: true,
+        dots: true,
+        slidesToShow: 3,
+    });
 });
